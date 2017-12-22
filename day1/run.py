c53b6901f74d30cce71f3623b6c4fa25d5e3ecc0
Kirk Hardy <megal33tdude@gmail.com>
2017-12-22 15:46:38 -0500

    day1 part 2

--- day1/run.py
+++ day1/run.py
@@ -1,8 +1,18 @@
 with open ("input.txt", "r") as myfile:
     inputString = myfile.readlines()[0].replace('\n', '')
     summation = []
-    inputString += inputString[0]
-    for i in range(0, len(inputString)-1):
-        if int(inputString[i]) == int(inputString[i+1]):
+    length = len(inputString)
+    print(inputString)
+    day2 = True
+    if day2:
+        elements_no = len(inputString)
+        elements_no = int(elements_no/2)
+    else:
+        elements_no = 1
+
+    inputString += inputString
+    or i in range(0, length):
+        #print("comparing " + inputString[i] + " and " + inputString[i+elements_no])
+        if int(inputString[i]) == int(inputString[i + elements_no]):
             summation.append(int(inputString[i]))
     print(sum(summation)) 
