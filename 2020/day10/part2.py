ef7e36012708cc22623b9fad36fd4efd2334f6cb
thingscouldbeworse <megal33tdude@gmail.com>
2020-12-14 22:04:43 -0500

    don't need that

--- 2020/day10/part2.py
+++ 2020/day10/part2.py
@@ -1,7 +1,7 @@
 import math
 
 data = []
-with open("test.txt", "r") as inputdata:
+with open("input.txt", "r") as inputdata:
   data.insert(0,0)
   for line in inputdata:
     data.append(int(line[:-1]))
@@ -11,22 +11,18 @@ data.append(data[-1] + 3)
 print(data)
 
 i = 1
-nodes = []
-num_alt_nodes = 0
+node_count = 0
+possibilities = 1
 while i <= len(data) - 2:
-  if i + 1 < len(data) and (data[i+1] - data[i-1] <= 3):
-    node = [data[i]] 
-    if i + 2 < len(data) and data[i+2] - data[i-1] <= 3:
-      num_alt_nodes += 1
-      node.append(data[i+1])
-      i += 1
-    nodes.append(node)
+  if data[i+1] - data[i-1] <= 3:
+    node_count += 1
+  else:
+    if node_count == 3:
+      possibilities = possibilities * 7
+    elif node_count > 0:
+      possibilities = possibilities * (2 * node_count)
+    node_count = 0
+  
   i += 1
   
-  
-print(nodes)
-print(len(nodes))
-print(num_alt_nodes)
-
-#paths = int(math.pow(2, num_pure_nodes)) + num_duo_nodes + (num_duo_nodes * num_pure_nodes) + 1
-#print(paths)
+print(possibilities)
